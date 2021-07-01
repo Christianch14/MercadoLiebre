82f11c55d06eb76445290ceb9a3a46d9080497a2
Christianch14 <christiano012010@gmail.com>
2021-07-01 14:21:52 -0500

    Proytecto agregado

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3000, ()=>{
+    console.log("Servidor 3000 corriendo")
+})
+
+app.use(express.static('Public'));
+
+app.use(express.static('Views'));
+
+
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, "./Views/home.html"));
+})
+
+app.get('/register', function(req, res){
+	res.sendFile(path.join(__dirname, "register.html"));
+})
+
+app.get('/login', function(req, res){
+	res.sendFile(path.join(__dirname, "login.html"));
+})
+
